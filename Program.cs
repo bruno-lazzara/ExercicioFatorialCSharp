7f393e9259adbf0efa4c47151536939a83d7bc74
Bruno Lazzara Queiroz <34097455+bruno-lazzara@users.noreply.github.com>
2022-02-10 01:36:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int fatorial = 1;
+
+            while (n <= 10)
+            {
+                fatorial *= n;
+                Console.WriteLine("O fatorial de " + n + " = " + fatorial);
+                n++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
